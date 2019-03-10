f3d0a795e471be762a82ef3b8380db61ffd10a05
eccx400 <echeng@scu.edu>
2019-03-09 16:20:33 -0800

    Checked

--- web/migrations/007_create_partOrders.sql
+++ web/migrations/007_create_partOrders.sql
@@ -1,5 +1,5 @@
 CREATE TABLE part_orders (
 	part_name VARCHAR(20) NOT NULL FOREIGN KEY REFERENCES parts(name),
-	repair_id INT(20) NOT NULL FOREIGN KEY REFERENCES repair_job(repair_id),
-	quantity INT(20) NOT NULL
+	repair_id INT(20)     NOT NULL FOREIGN KEY REFERENCES repair_job(repair_id),
+	quantity  INT(20)     NOT NULL
 )
