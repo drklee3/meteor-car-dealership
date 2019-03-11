6495fbc740309cb51ede290cf308bfd9ab99cdc3
mikaphilip <mikaphilip21@gmail.com>
2019-03-10 20:14:43 -0700

    parts_and_mechanics.sql

---
+++ web/sql/parts_and_mechanics.sql
@@ -0,0 +1,24 @@
+insert into mechanic values('Rosa Parks', 30.50);
+insert into mechanic values('Derrick Lee', 20.75);
+insert into mechanic values('Mika Philip', 15);
+insert into mechanic values('Eric Cheng', 16.25);
+insert into mechanic values('Gourdon Ramsey', 30.51);
+
+
+insert into parts values('Tire', 150);
+insert into parts values('antifreeze', 25.50);
+insert into parts values('Hubcap', 230);
+insert into parts values('Window Motor', 240);
+insert into parts values('Window Regulator', 150);
+insert into parts values('Oil', 15.75);
+insert into parts values('Brake Fluid', 30.40);
+insert into parts values('Windshield', 34);
+insert into parts values('Hinge', 80);
+insert into parts values('Door Lock', 75);
+insert into parts values('Car Battery', 140);
+insert into parts values('Headlight Bulb', 120.50);
+insert into parts values('Inside Bulb', 56.25);
+insert into parts values('Antenna', 65.40);
+insert into parts values('Ignition Coil', 80);
+insert into parts values('Spark Plug', 50.70);
+insert into parts values('Window Seal', 40.25);
