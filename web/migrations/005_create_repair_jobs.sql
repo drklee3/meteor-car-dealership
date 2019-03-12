c04fde9a203f9179ad08715c04ed2f9501d9a56d
eccx400 <echeng@scu.edu>
2019-03-12 11:06:56 -0700

    Change time in datatype and added labour hours to Repair jobs

--- web/migrations/005_create_repair_jobs.sql
+++ web/migrations/005_create_repair_jobs.sql
@@ -2,8 +2,9 @@ CREATE table repair_jobs(
 	repair_id     INT         PRIMARY KEY,
 	emp_id        INT         NOT NULL,
 	licence_no    VARCHAR(20) NOT NULL,
-	time_in       YEAR        NOT NULL,
-	time_out      YEAR,
+	labour_hours  INT         NOT NULL,
+	time_in       TIMESTAMP   NOT NULL,
+	time_out      TIMESTAMP,
 	CONSTRAINT emp_id_fk     FOREIGN KEY(emp_id)
 		REFERENCES mechanics(emp_id),
 	CONSTRAINT licence_no_fk FOREIGN KEY(licence_no)
--- web/migrations/006_create_repair_problems.sql
+++ web/migrations/006_create_repair_problems.sql
@@ -1,6 +1,6 @@
 CREATE TABLE repair_problems(
-	problem_id INT NOT NULL,
-	repair_id  INT NOT NULL,
+	problem_id   INT NOT NULL,
+	repair_id    INT NOT NULL,
 	CONSTRAINT repair_pk  PRIMARY KEY(problem_id, repair_id),
 	CONSTRAINT problem_fk FOREIGN KEY(problem_id)
 			REFERENCES problems(id),
