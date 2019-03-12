d588bb44e18d9c1bda7eca1e4f0161fc8918a364
eccx400 <echeng@scu.edu>
2019-03-11 22:36:00 -0700

    Change times to YEAR

--- web/migrations/005_create_repair_jobs.sql
+++ web/migrations/005_create_repair_jobs.sql
@@ -2,8 +2,8 @@ CREATE table repair_jobs(
 	repair_id     INT         PRIMARY KEY,
 	emp_id        INT         NOT NULL,
 	licence_no    VARCHAR(20) NOT NULL,
-	time_in       VARCHAR(10) NOT NULL,
-	time_out      VARCHAR(10),
+	time_in       YEAR        NOT NULL,
+	time_out      YEAR,
 	CONSTRAINT emp_id_fk     FOREIGN KEY(emp_id)
 		REFERENCES mechanics(emp_id),
 	CONSTRAINT licence_no_fk FOREIGN KEY(licence_no)
