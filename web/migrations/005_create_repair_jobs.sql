51d779c7a016f7cce7a86e89a07a389ca3c904bd
eccx400 <echeng@scu.edu>
2019-03-11 22:23:30 -0700

    Change variable data type according to query requirements

--- web/migrations/005_create_repair_jobs.sql
+++ web/migrations/005_create_repair_jobs.sql
@@ -1,7 +1,7 @@
 CREATE table repair_jobs(
 	repair_id     INT         PRIMARY KEY,
 	emp_id        INT         NOT NULL,
-	licence_no    INT         NOT NULL,
+	licence_no    VARCHAR(20) NOT NULL,
 	time_in       VARCHAR(10) NOT NULL,
 	time_out      VARCHAR(10),
 	CONSTRAINT emp_id_fk     FOREIGN KEY(emp_id)
