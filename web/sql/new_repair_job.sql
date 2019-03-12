ba08da25c5aa983a7c9cfc304b1a3735f23d834d
mikaphilip <mikaphilip21@gmail.com>
2019-03-12 14:00:57 -0700

    yet more revisions (merged functions)

--- web/sql/new_repair_job.sql
+++ web/sql/new_repair_job.sql
@@ -11,6 +11,27 @@ DECLARE
 	prob_id number;
 	problem_count number;
 	pname varchar(20);
+	CREATE OR REPLACE PROCEDURE assign_problem_id (current_problem_type)
+	AS
+		new_problem_id := 1;
+	BEGIN 
+		SELECT max(problem_id) 
+			INTO new_problem_id 
+			FROM problems;
+		FOR i IN 1..dbms_random.values(1,5) LOOP
+			INSERT INTO problems(new_problem_id+i, current_problem_type);
+	END assign_problem_id;
+	CREATE OR REPLACE PROCEDURE assign_repair_id
+	AS
+	RETURN number IS
+		new_repair_id := 0;
+	BEGIN 
+		SELECT max(repair_id) 
+			into new_repair_id 
+			from repair_job;
+		new_repair_id := new_repair_id + 1;
+		RETURN new_repair_id;
+	END assign_repair_id;
 	PROCEDURE insertArray(arr IN array_t, repairId repair_jobs.repair_id%type) 
 	IS
 	BEGIN
@@ -23,7 +44,7 @@ DECLARE
 					VALUES (repairId,arr(i));
 		END LOOP;
 	END insertArray;
-	FUNCTION insert_problems (problem_types in array_t, repairId) RETURN 
+	FUNCTION insert_problems (problem_types in array_t, repairId repair_jobs.repair_id%type) RETURN 
 	IS NUMBER num_prob
 	BEGIN
 		SELECT count(*) 
