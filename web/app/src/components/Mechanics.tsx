b7910eaedaa070eafec2440a7ba5c8a333f22c92
drklee3 <dlee3@scu.edu>
2019-03-13 11:38:15 -0700

    add mechanics component placeholder

---
+++ web/app/src/components/Mechanics.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+// import * as request from "../request";
+
+export default class Mechanics extends React.Component {
+    public render() {
+        return (
+            <div>
+                hello
+            </div>
+        );
+    }
+}
