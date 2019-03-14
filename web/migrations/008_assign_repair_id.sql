b980cbee8c004171fb364337ad1d8fefe93686c7
eccx400 <echeng@scu.edu>
2019-03-13 22:52:02 -0700

    Check 008 works need 009 fix

--- web/migrations/008_assign_repair_id.sql
+++ web/migrations/008_assign_repair_id.sql
@@ -1,14 +1,12 @@
 --find max repair_id, then + 1
-CREATE OR REPLACE PROCEDURE assign_repair_id
+CREATE OR REPLACE FUNCTION assign_repair_id
+RETURN NUMBER
 AS
-RETURN number IS
-	new_repair_id := 0;
+	new_repair_id INTEGER := 0;
 BEGIN 
 	SELECT max(repair_id) 
-		into new_repair_id 
-		from repair_job;
+		INTO new_repair_id 
+		FROM repair_jobs;
 	new_repair_id := new_repair_id + 1;
 	RETURN new_repair_id;
-END;
-/
-show errors;	
+END;	
