5b5f5d29f9dc091de42d2665665351d7056022a8
eccx400 <echeng@scu.edu>
2019-03-15 12:46:21 -0700

    Modified bill

--- web/sql/get_bill.sql
+++ web/sql/get_bill.sql
@@ -1,3 +1,6 @@
+DECLARE
+	total NUMBER(100,2);
+BEGIN
       SELECT customers.name
              , address
              , customers.phone
@@ -7,10 +10,25 @@
              , repair_jobs.time_in
              , repair_jobs.time_out
              , labour_hours * hourly_pay + 30
+	     , parts.cost
                AS cost
                -- but we need cost of parts here too
         FROM repair_jobs
 NATURAL JOIN customers
 NATURAL JOIN cars
+NATURAL JOIN parts
         JOIN mechanics
           ON repair_jobs.emp_id = mechanics.emp_id
+
+	SELECT total
+	FROM
+	(
+		-- total cost?
+		SELECT labour_costs * hourly_pay + 30
+		FROM mechanics
+		NATURAL JOIN parts
+		UNION ALL 
+		SELECT cost
+		FROM parts
+	)
+END;
